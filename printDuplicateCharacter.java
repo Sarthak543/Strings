3a0e0c70fad454c893f05e011613d997ddd5fa0b
Sarthak543 <sarthakjaiswal543@gmail.com>
2023-02-16 19:46:05 +0530

    new operation added

--- printDuplicateCharacter.java
+++ printDuplicateCharacter.java
@@ -1,5 +1,25 @@
 public class printDuplicateCharacter {
+    String printDupliChar(String str1) {
+        String str2 = "";
+        String duplicate = "";
+        boolean flag = true; // true means duplicate not present
+        for (int i = 0; i < str1.length(); i++) {
+            for (int j = 0; j < str2.length(); j++) {
+                if (str1.charAt(i) == str2.charAt(j)) {
+                    duplicate = duplicate + str1.charAt(i);
+                    flag=false;
+                }
+            }
+           if (flag) {
+            str2=str2+str1.charAt(i);
+           }
+        }
+        return duplicate;
+    }
+
     public static void main(String[] args) {
-        
+      String str="sarthak jaiswal";
+      printDuplicateCharacter pdc = new printDuplicateCharacter();
+      System.out.println(pdc.printDupliChar(str));
     }
 }
