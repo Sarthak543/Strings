81f9991f3c61ecf1d42498abfdae7aaf93da2df3
Sarthak543 <sarthakjaiswal543@gmail.com>
2023-02-16 07:22:42 +0530

    reverseWord operation added

--- reversingString.java
+++ reversingString.java
@@ -6,15 +6,29 @@ class StringOperation {
         }
         return str2;
     }
+
+    String reverseWord(String str1){
+        String str2="";
+        String arr[]=str1.split(" ");
+        for (int i = arr.length-1; i >= 0; i--) {
+            str2=str2+arr[i]+" ";
+        }
+        return str2;
+    }
 }
 
 public class reversingString {
 
     public static void main(String[] args) {
         StringOperation so = new StringOperation();
-        String name = "Sarthak";
+        String name = "Sarthak Jaiswal";
+        String name1 = "Sarthak Jaiswal";
         System.out.println("before reversing : "+name);
         name = so.reverse(name);
         System.out.println("after reversing : "+name);
+
+        System.out.println("before reversing word : "+name1);
+        name1 = so.reverseWord(name1);
+        System.out.println("after reversing word: "+name1);
     }
 }
